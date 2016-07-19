478397c28900c2dfcd5afa0d5e5971f66a2a4d92
CadenArmstrong <cadenarmstrong@gmail.com>
2016-07-19 11:41:27 -0400

    Added utilities for tarfile splitting

--- ia_split.py
+++ ia_split.py
@@ -1,4 +1,7 @@
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET      # To read the TOC and other xml files
+import glob                             # To get lists of files from folders
+from subprocess import call             # To tar and untar files
+import shutil
 
 def split_by_chapter(xmlfile):
     """ Split the xml file by an identifier into multiple xml files
@@ -30,9 +33,64 @@ def create_new_book_xml(subtrees):
 
     return root                     # Return new book
 
+def move_file(orig,destfolder):
+    """ The dest folder doesn't include the filename, while orig does"""
+
+    shutil.move(orig,destfolder+"/"+orig.split("/")[-1])
+
+def untarball(tarfile, dest):
+    """Untar a file
+       creates path to dest if it doesn't exist"""
+    call(['mkdir','-p',dest])
+    call(['tar','-xzf',tarfile,"-C",dest])
+
+def create_dest_folders(name,range_start,range_end,padding):
+    """ Range_start inclusive, range_end exclusive
+        padding is zero padding on the number
+        ex. 001 is 3, 0001 is 4...etc
+
+        returns the list of folders created
+    """
+
+    number_format = "%0"+str(padding)+"d"
+    folders = []
+    for a in range(range_start,range_end):
+        call(['mkdir','-p',name+number_format%(a)])
+        folders.append(name+number_format%(a))
+    return folders
+
+def split_into_folders(tarfile_name,toc,tempfolder="_tmp"):
+    """Untar the tarball file and using the table of contents, split the tarfile into multiple folders
+      
+      tarfile -> STRING file name and path of the tarfile to be split into sub folders
+      toc -> STRING file name and path of the TOC.xml file to use for splitting the tarfile
+      tempfolder -> path of a temp folder ?
+       
+       """
+
+    untarball(tarfile_name,tempfolder) # Untar the image files
+
+    files = glob.glob(tempfolder+"/*.jp2") # Get a list of the image files that have been uncompressed
+
+
+    
+    toc = ET.parse(xmlfile)                             # Open the TOC to  read for splitting
+    for page in toc.find('pageData').findall('chapter'): # TODO get the right xml tags to loop over
+        if('leafNum' in page.attrib and page.attrib['leafNum'] < count):
+            # Up count of some sort? 
+            pass # POP image into directory?
+
+
 if __name__ == "__main__":
     
-    split_by_chapter("eightconceptsofb00gilb/eightconceptsofb00gilb_scandata.xml")
+#    split_by_chapter("eightconceptsofb00gilb/eightconceptsofb00gilb_scandata.xml")
+    
+# untarball("testarchive/testarchive.tar","newarchive")
+#    split_into_folders("testarchive/testarchive.tar","TOC.xml","newarchive")
+#    move_file("testarchive/testfile.txt","newarchive")
+
+#    create_dest_folders("bleh",0,11,0)
 
 
 
+    pass
